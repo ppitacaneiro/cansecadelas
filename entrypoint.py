09f376524ff12c2a5fd4469611ef54c4aad04269
Pablo Pita <ppitacaneiro@gmail.com>
2024-03-23 11:21:51 +0100

    handle exceptions in scrapper process

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,6 @@
 import os
 import click
+import requests
 from app import create_app
 from app.petsadopt.models import Pet
 from app.scrappers.scrapper_factory import ScrapperFactory
@@ -15,14 +16,22 @@ def scrapper():
     url = 'https://refugiocambados.es/adopta/animales-en-adopcion/'
     name = 'cambados'
     
-    scrapper = ScrapperFactory.get_scrapper(name, url)
-    pets = scrapper.scrap()
-    for pet in pets:
-        print(pet)
-        new_pet = Pet(
-            name=pet['pet_name'],
-            image_src=pet['url_image'],
-            url_adopt=pet['url_website'],
-        )
-        new_pet.save()
-    print("Scrapper command finished")
+    try:
+        scrapper = ScrapperFactory.get_scrapper(name, url)
+        pets = scrapper.scrap()
+        for pet in pets:
+            print(pet)
+            new_pet = Pet(
+                name=pet['pet_name'],
+                image_src=pet['url_image'],
+                url_adopt=pet['url_website'],
+            )
+            new_pet.save()
+    except requests.exceptions.RequestException as e:
+        print(f"Unable to connect to the website: {e}")
+    except Exception as e:
+        print(f"An error ocurred: {e}")
+    else:   
+        print("Scrapper command finished successfully")
+    finally:
+        print("Scrapper command finished")
---
+++ readme.md
@@ -0,0 +1,9 @@
+# activate the virtual enviroment
+source env/bin/activate
+
+# flask in dev mode
+flask run --debug
+
+# scrapping command
+flask scrapper
+
